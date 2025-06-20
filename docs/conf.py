c78d43cb3196effa2753d6aa5a55f47d684375a0
Amaya William <amayaw376@gmail.com>
2025-06-20 11:26:36 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+
+project = 'NordPass Password Manager'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
